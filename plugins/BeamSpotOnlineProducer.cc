84c8b2dc0269854538f9054ad808511109564e11
cmsbuild <>
2010-05-03 11:41:30 +0000

    This commit was manufactured by cvs2git to create tag 'RecoVertex- BeamSpotProducer-CMSSW_3_8_0_pre1'.
    Sprout from master 2010-04-23 20:50:56 UTC Francisco Xavier Yumiceva Del Pozo <sha1-d7feb986bbb8c90e95e5ffd6abf676c1f06ed388@cern.ch> 'remove scalers from configs'
    Cherrypick from master 2010-05-03 11:41:05 UTC Jean-Roch Vlimant <sha1-c20eea45fcd90cd8b9272a4189992d8eff37fabb@cern.ch> 'protect for empty online beamspot collection : no warning issued':
        interface/BeamSpotOnlineProducer.h
        plugins/BeamSpotOnlineProducer.cc
        python/BeamSpotOnline_cfi.py
        src/BSFitter.cc

--- plugins/BeamSpotOnlineProducer.cc
+++ plugins/BeamSpotOnlineProducer.cc
@@ -20,6 +20,12 @@ BeamSpotOnlineProducer::BeamSpotOnlineProducer(const ParameterSet& iconf)
   scalertag_ = iconf.getParameter<InputTag>("label");
   changeFrame_ = iconf.getParameter<bool>("changeToCMSCoordinates");
 
+  theMaxR2 = iconf.getParameter<double>("maxRadius");
+  theMaxR2*=theMaxR2;
+  theMaxZ = iconf.getParameter<double>("maxZ");
+
+  theSetSigmaZ = iconf.getParameter<double>("setSigmaZ");
+
   produces<reco::BeamSpot>();
 
 } 
@@ -37,50 +43,72 @@ BeamSpotOnlineProducer::produce(Event& iEvent, const EventSetup& iSetup)
   // beam spot scalar object
   BeamSpotOnline spotOnline;
 
-  // get one element
-  spotOnline = * ( handleScaler->begin() );
-
   // product is a reco::BeamSpot object
   std::auto_ptr<reco::BeamSpot> result(new reco::BeamSpot);
-
+  
   reco::BeamSpot aSpot;
 
-  // in case we need to switch to LHC reference frame
-  // ignore for the moment rotations, and translations
-  double f = 1.;
-  if (changeFrame_) f = -1.;
-
-  reco::BeamSpot::Point apoint( f* spotOnline.x(), spotOnline.y(), f* spotOnline.z() );
-
-  reco::BeamSpot::CovarianceMatrix matrix;
-  matrix(0,0) = spotOnline.err_x()*spotOnline.err_x();
-  matrix(1,1) = spotOnline.err_y()*spotOnline.err_y();
-  matrix(2,2) = spotOnline.err_z()*spotOnline.err_z();
-  matrix(3,3) = spotOnline.err_sigma_z()*spotOnline.err_sigma_z();
-
-  aSpot = reco::BeamSpot( apoint,
-			  spotOnline.sigma_z(),
+  bool fallBackToDB=false;
+  if (handleScaler->size()!=0){
+    // get one element
+    spotOnline = * ( handleScaler->begin() );
+    
+    // in case we need to switch to LHC reference frame
+    // ignore for the moment rotations, and translations
+    double f = 1.;
+    if (changeFrame_) f = -1.;
+    
+    reco::BeamSpot::Point apoint( f* spotOnline.x(), spotOnline.y(), f* spotOnline.z() );
+    
+    reco::BeamSpot::CovarianceMatrix matrix;
+    matrix(0,0) = spotOnline.err_x()*spotOnline.err_x();
+    matrix(1,1) = spotOnline.err_y()*spotOnline.err_y();
+    matrix(2,2) = spotOnline.err_z()*spotOnline.err_z();
+    matrix(3,3) = spotOnline.err_sigma_z()*spotOnline.err_sigma_z();
+    
+    double sigmaZ = spotOnline.sigma_z();
+    if (theSetSigmaZ>0)
+      sigmaZ = theSetSigmaZ;
+    
+    aSpot = reco::BeamSpot( apoint,
+			    sigmaZ,
 			  spotOnline.dxdz(),
-			  f* spotOnline.dydz(),
-			  spotOnline.width_x(),
-			  matrix);
-
-  aSpot.setBeamWidthY( spotOnline.width_y() );
-  aSpot.setEmittanceX( 0. );
-  aSpot.setEmittanceY( 0. );
-  aSpot.setbetaStar( 0.) ;
-  aSpot.setType( reco::BeamSpot::LHC ); // flag value from scalars
-
-  // check if we have a valid beam spot fit result from online DQM
-
-  if ( spotOnline.x() == 0 &&
-       spotOnline.y() == 0 &&
-       spotOnline.z() == 0 &&
-       spotOnline.width_x() == 0 &&
-       spotOnline.width_y() == 0 ) {
-
-    //edm::LogInfo("RecoVertex/BeamSpotProducer") 
-    //<< "Online Beam Spot producer fall back to DB value " << "\n";
+			    f* spotOnline.dydz(),
+			    spotOnline.width_x(),
+			    matrix);
+    
+    aSpot.setBeamWidthY( spotOnline.width_y() );
+    aSpot.setEmittanceX( 0. );
+    aSpot.setEmittanceY( 0. );
+    aSpot.setbetaStar( 0.) ;
+    aSpot.setType( reco::BeamSpot::LHC ); // flag value from scalars
+    
+    // check if we have a valid beam spot fit result from online DQM
+    if ( spotOnline.x() == 0 &&
+	 spotOnline.y() == 0 &&
+	 spotOnline.z() == 0 &&
+	 spotOnline.width_x() == 0 &&
+	 spotOnline.width_y() == 0 ) 
+      {
+	edm::LogWarning("BeamSpotFromDB") 
+	  << "Online Beam Spot producer falls back to DB value because the scaler values are zero ";
+	fallBackToDB=true;
+      }
+    double r2=spotOnline.x()*spotOnline.x() + spotOnline.y()*spotOnline.y();
+    if (fabs(spotOnline.z())>=theMaxZ || r2>=theMaxR2){
+      edm::LogError("BeamSpotFromDB") 
+	<< "Online Beam Spot producer falls back to DB value because the scaler values are too big to be true :"
+	<<spotOnline.x()<<" "<<spotOnline.y()<<" "<<spotOnline.z();
+      fallBackToDB=true;
+    }
+  }
+  else{
+    //empty online beamspot collection: FED data was empty
+    //the error should probably have been send at unpacker level
+    fallBackToDB=true;
+  }
+      
+  if (fallBackToDB){
 
     edm::ESHandle< BeamSpotObjects > beamhandle;
     iSetup.get<BeamSpotObjectsRcd>().get(beamhandle);
